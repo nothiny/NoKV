049954bd4c2c9011d0035d4221661f6457891e18
nothiny <nothiny@users.noreply.github.com>
2026-03-04 07:40:07 +0000

    deploy: e3efa6fca30ef5ab77d24fbd8c3f2d2e70d1075a

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-de60993c.js',
+                path_to_root + 'searchindex-f5b0fed8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
